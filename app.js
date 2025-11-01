d7b9b9321ac32371f263de18878259b0c218ab2b
davidtranchaud79-svg <david.tranchaud79@gmail.com>
2025-11-01 16:05:29 +0100

    Update API_URL to new endpoint

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 // Fouquet’s Joy Suite – v14.9.2 (Gold Motion Clean Edition)
-export const API_URL = localStorage.getItem('API_BASE') || "https://script.google.com/macros/s/AKfycbzvkGlQiIzsfgxGXrem_qGrrI6QmBDe3Zw0G1bHeIA8CBTtuJEeG1_xDbFaiKYzt8hHDg/exec";
+export const API_URL = localStorage.getItem('API_BASE') || "https://script.google.com/macros/s/AKfycbx6Sw2M_uZe8CPr_XSadqWZsP_-AnyrD3uZFwv4hAmdoCxJ2pooc5hBchNCSc5SPqv3cQ/exec";
 
 const qs = (s, r=document) => r.querySelector(s);
 const qsa = (s, r=document) => [...r.querySelectorAll(s)];
