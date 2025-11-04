a6b1d2fabcc6306ddf090bc1006adf542a0e0204
davidtranchaud79-svg <david.tranchaud79@gmail.com>
2025-11-04 12:50:22 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,18 +1,16 @@
-const CACHE_NAME="fouquets-suite-v15.3";
-const FILES_TO_CACHE=["./","./index.html","./app.js","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
-self.addEventListener("install",e=>{
-  e.waitUntil(caches.open(CACHE_NAME).then(c=>c.addAll(FILES_TO_CACHE)));
-  self.skipWaiting();
+const CACHE_NAME = 'fouquets-v15';
+const FILES = [
+  './',
+  './index.html',
+  './style.css',
+  './app.js',
+  './manifest.json'
+];
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
 });
-self.addEventListener("activate",e=>{
-  e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE_NAME?caches.delete(k):null))));
-  self.clients.claim();
-});
-self.addEventListener("fetch",e=>{
-  if(e.request.method!=="GET")return;
+self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{
-      return caches.open(CACHE_NAME).then(c=>{c.put(e.request.url,res.clone());return res});
-    }))
+    caches.match(e.request).then(r => r || fetch(e.request))
   );
 });
